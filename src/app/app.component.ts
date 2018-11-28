413f6e14a18340d58325a95a249096d25f6e7118
YassineBenaziz <yassin.benaziz@gmail.com>
2018-11-28 14:17:58 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component,OnInit } from '@angular/core';
+import { Pokemon } from './pokemon';
+import { POKEMONS } from './mock-pokemons';
+
+@Component({
+  selector: 'pokemon-app',
+  template: `<h1>Hello youssef {{name}}</h1>`,
+})
+export class AppComponent implements OnInit  { 
+  
+  name = 'Angular';
+
+  private pokemons:Pokemon[]=null; 
+
+  ngOnInit(){
+    this.pokemons = POKEMONS;
+  }
+
+  selectPokemon(pokemon:Pokemon){
+    alert("Vous avez cliqué sur "+pokemon.name);
+  }
+
+
+}
