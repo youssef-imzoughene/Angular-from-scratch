6b8b88dde47c5ef9dd0f39b304fc32fb90001fd5
YassineBenaziz <yassin.benaziz@gmail.com>
2018-11-28 17:14:45 +0100

    custom pipes

--- src/dist/app/app.module.js
+++ src/dist/app/app.module.js
@@ -10,13 +10,14 @@ var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
 var app_component_1 = require("./app.component");
 var border_card_directive_1 = require("./border-card.directive");
+var pokemon_type_color_pipe_1 = require("./pokemon-type-color.pipe");
 var AppModule = /** @class */ (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         core_1.NgModule({
             imports: [platform_browser_1.BrowserModule],
-            declarations: [app_component_1.AppComponent, border_card_directive_1.BorderCardDirective],
+            declarations: [app_component_1.AppComponent, border_card_directive_1.BorderCardDirective, pokemon_type_color_pipe_1.PokemonTypeColorPipe],
             bootstrap: [app_component_1.AppComponent]
         })
     ], AppModule);
